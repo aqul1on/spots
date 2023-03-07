37f7effecdcb87a16d22d6416a9985fd1a3b8e58
aqul1on <111996543+aqul1on@users.noreply.github.com>
2023-03-07 09:04:47 +0500

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -21,6 +21,8 @@ public partial class Form1 : Form
         //InitializeTimer();
         ConfigForm();
         InitializeGame();
+        FieldMixing();
+        IsFieldMixingCorrect();
     }
     public void ConfigForm()
     {
@@ -45,7 +47,7 @@ public partial class Form1 : Form
                 buttonGrid[row, col].Size = new System.Drawing.Size(buttonSize, buttonSize);
                 buttonGrid[row, col].Location = new System.Drawing.Point(leftPadding + col * buttonSize, topPadding + row * buttonSize); // формула для заполнения поля 
                 buttonGrid[row, col].Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-                buttonGrid[row, col].Click += new EventHandler(Button_Click); // устанавливает обработчик события Click
+                buttonGrid[row, col].Click += new EventHandler(ButtonClick); // устанавливает обработчик события Click
                 // Button_Click: это имя метода, который будет вызываться при возникновении события
                 buttonGrid[row, col].BackgroundImage = Image.FromFile("E:\\1\\jsegF14iwJs.jpg");
                 this.Controls.Add(buttonGrid[row, col]);
@@ -60,13 +62,13 @@ public partial class Form1 : Form
         this.Controls.Add(movesLabel);
         
         Button btnRestart = new Button();
-        btnRestart.Width = Convert.ToInt32(buttonSize * 0.9);
-        btnRestart.Height = Convert.ToInt32(buttonSize * 0.9);
+        btnRestart.Width = Convert.ToInt32(buttonSize);
+        btnRestart.Height = Convert.ToInt32(buttonSize);
         btnRestart.Text = "Reload";
         btnRestart.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
         btnRestart.BackgroundImage = Image.FromFile("E:\\1\\jsegF14iwJs.jpg");
         btnRestart.Location = new Point( 0 , 0); // задайте координаты расположения кнопки на форме
-        btnRestart.Click += new EventHandler(btnRestart_Click);
+        btnRestart.Click += new EventHandler(BtnRestartClick);
         this.Controls.Add(btnRestart);
         
         // создаем массив для хранения текущего расположения квадратиков ( от 1 до 15 )
@@ -79,7 +81,24 @@ public partial class Form1 : Form
             }
         }
         gameBoard[boardSize - 1, boardSize - 1] = 0; // последний квадратик без цифры
+        
+    }
 
+    private void IsFieldMixingCorrect()
+    {
+        FieldMixing();
+        if (IsSolvable(gameBoard))
+        {
+            UpdateButtons();
+        }
+        else
+        {
+            IsFieldMixingCorrect();
+        }
+    }
+
+    private void FieldMixing()
+    {
         // перемешиваем квадратики в случайном порядке ( меняем расположение) 
         Random rand = new Random();
         for (int i = 0; i < 1000; i++)
@@ -93,9 +112,8 @@ public partial class Form1 : Form
             gameBoard[row2, col2] = temp;
             
         }
-        UpdateButtons();
     }
-    private void btnRestart_Click(object sender, EventArgs e)
+    private void BtnRestartClick(object sender, EventArgs e)
     {
         // перемешиваем квадратики в случайном порядке ( меняем расположение) 
         Random rand = new Random();
@@ -136,7 +154,36 @@ public partial class Form1 : Form
         
     }
     
-    private void Button_Click(object sender, EventArgs e)
+    private bool IsSolvable(int[,] board)
+    {
+        // Преобразовать двумерный массив в одномерный массив для удобства обработки
+        int[] flatBoard = new int[board.Length];
+        for (int i = 0; i < board.GetLength(0); i++)
+        {
+            for (int j = 0; j < board.GetLength(1); j++)
+            {
+                flatBoard[i * board.GetLength(1) + j] = board[i, j];
+            }
+        }
+
+        // Посчитать число инверсий
+        int inversions = 0;
+        for (int i = 0; i < flatBoard.Length - 1; i++)
+        {
+            for (int j = i + 1; j < flatBoard.Length; j++)
+            {
+                if (flatBoard[i] > flatBoard[j] && flatBoard[i] != 16 && flatBoard[j] != 16)
+                {
+                    inversions++;
+                }
+            }
+        }
+
+        // Проверить, является ли число инверсий четным
+        return inversions % 2 == 0;
+    }
+    
+    private void ButtonClick(object sender, EventArgs e)
     {
         // находим нажатую кнопку и её координаты на игровом поле
         Button clickedButton = (Button)sender;
@@ -227,5 +274,6 @@ public partial class Form1 : Form
     }
     
 }
+    
     
     
